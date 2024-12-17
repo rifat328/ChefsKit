0b6ae0121497f8245378a1d60de258b005155d5d
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-12-17 23:26:32 +0600

    Updated Entry

--- chefskit/src/component/Entry.jsx
+++ chefskit/src/component/Entry.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import "../styling/entry.css";
 const Entry = () => {
-  const ingredients = ["chicken", "oregano", "Tomatoes"];
-  const ingredientList = ingredients.map((ingredient) => <li>{ingredient}</li>);
-  const submitOutput = () => console.log("input is submitted");
+  const [ingredients, setIngredient] = React.useState([]);
+  const ingredientList = ingredients.map((ingredient) => (
+    <li style={{ listStyleType: "none" }}>{ingredient}</li>
+  ));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredient((prevIngredient) => [...prevIngredient, newIngredient]);
+  };
+
   return (
     <main>
-      <form className="add-ingredient-form" onSubmit={submitOutput}>
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="e.g chicken"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
-      <ul>{ingredientList}</ul>
+      <ul className="ingredient-list" aria-label="ingredient list">
+        {ingredientList}
+      </ul>
     </main>
   );
 };
