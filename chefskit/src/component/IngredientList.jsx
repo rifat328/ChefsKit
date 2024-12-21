6cc1c4ca69d1139c1e5e5195f6e740223d31bfb4
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-12-21 06:55:39 +0600

    Recipe got from api finished and printed on console

---
+++ chefskit/src/component/IngredientList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const IngredientList = (props) => {
+  const ingredientsListItems = props.ingredients.map((ingredient) => (
+    <li key={ingredient} style={{ listStyleType: "none" }}>
+      {ingredient}
+    </li>
+  ));
+  return (
+    <section>
+      <h2>Ingredients on hand:</h2>
+      <ul className="ingredients-list" aria-live="polite">
+        {ingredientsListItems}
+      </ul>
+      {props.ingredients.length >= 4 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={props.getRecipe}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default IngredientList;
